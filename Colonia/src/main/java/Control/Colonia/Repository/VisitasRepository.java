6ca0b70bc989ba34b68c33a6cd02594ba77ddaa9
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-21 11:52:59 -0600

    feat: se hicieron cambios en VisitasRepository

--- Colonia/src/main/java/Control/Colonia/Repository/VisitasRepository.java
+++ Colonia/src/main/java/Control/Colonia/Repository/VisitasRepository.java
@@ -1,6 +1,6 @@
-package com.Administracion.Colonia.Repository;
+package Control.Colonia.Repository;
 
-import com.Administracion.Colonia.Entity.Visitas;
+import Control.Colonia.Entity.Visitas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
