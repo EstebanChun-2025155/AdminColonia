7c2482fe76e84881b33d7331b5e9db91ef0fc680
echun-20025155 <echun-2025155@kinal.edu.gt>
2026-04-21 10:55:21 -0600

    feat: Se establecio conexión con la base de datos

--- Colonia/src/main/java/Control/Colonia/Service/CasaServiceImplements.java
+++ Colonia/src/main/java/Control/Colonia/Service/CasaServiceImplements.java
@@ -2,39 +2,43 @@ package Control.Colonia.Service;
 
 import Control.Colonia.Entity.Casa;
 import Control.Colonia.Repository.CasaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class CasaServiceImplements implements CasaService {
-    private final CasaRepository casaRepository;
+    @Autowired
+    private CasaRepository repo;
 
-    public CasaServiceImplements(CasaRepository casaRepository) {
-        this.casaRepository = casaRepository;
+    @Override
+    public List<Casa> getAllCasa() { return repo.findAll(); }
+
+    @Override
+    public Casa getCasaById(Integer id) {
+        return repo.findById(id).orElseThrow(() -> new RuntimeException("Casa no encontrada"));
     }
 
     @Override
-    public List<Casa> getAllCasa() { return casaRepository.findAll(); }
-
-    @Override
-    public Casa getCasaById(Integer id) { return casaRepository.getReferenceById(id); }
-
-    @Override
     public Casa saveCasa(Casa casa) throws RuntimeException {
-            return casaRepository.save(casa);
+            return repo.save(casa);
     }
 
     @Override
     public Casa updateCasa(Integer id, Casa casa) {
-        Casa existingCasa = casaRepository.findById(id).orElseThrow(() -> new RuntimeException("La casa no existe"));
-        return casaRepository.save(existingCasa);
+        Casa existingCasa = repo.findById(id).orElseThrow(() -> new RuntimeException("La casa no existe"));
+
+        existingCasa.setNoDeCasa(casa.getNoDeCasa());
+        existingCasa.setDireccion(casa.getDireccion());
+        existingCasa.setEstado(casa.getEstado());
+        existingCasa.setPropietario(casa.getPropietario());
+        existingCasa.setPrecioCasa(casa.getPrecioCasa());
+
+        return repo.save(existingCasa);
     }
 
     @Override
     public void deleteCasa(Integer id) {
-        if(!casaRepository.existsById(id)){
-            throw new RuntimeException("Este id no existe");
-        }
-        casaRepository.deleteById(id);
+        repo.deleteById(id);
     }
 }
