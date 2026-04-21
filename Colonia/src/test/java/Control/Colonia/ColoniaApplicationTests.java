4349dbf2ef53e43e7462a692e6761dd5b09c6a6f
echun-20025155 <echun-2025155@kinal.edu.gt>
2026-04-21 07:47:46 -0600

    feat: Se cargo la esrtucutura base del proyecto

---
+++ Colonia/src/test/java/Control/Colonia/ColoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package Control.Colonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
