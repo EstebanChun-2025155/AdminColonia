ff34795a2128421ba8c23de80b1a75449dc1c99e
echun-20025155 <echun-2025155@kinal.edu.gt>
2026-04-22 10:58:31 -0600

    refactor: se modifico conexión con la vista casa.html

--- Colonia/src/main/java/Control/Colonia/Controller/CasaController.java
+++ Colonia/src/main/java/Control/Colonia/Controller/CasaController.java
@@ -19,39 +19,41 @@ public class CasaController {
     public String mostrarCasa(Model model){
         List<Casa> lista = casaService.getAllCasa();
         model.addAttribute("casa", lista);
-
+        model.addAttribute("casaForm", new Casa());
         return "casa";
     }
 
     @PostMapping("/eliminar/casa/{id}")
     public String eliminar(@PathVariable Integer id){
         casaService.deleteCasa(id);
-        return "redirect/casa";
+        return "redirect:/casa";
     }
 
     @GetMapping("/nueva/casa")
     public String nuevaCasa(Model model){
-        model.addAttribute("casa", new Casa());
+        model.addAttribute("casaForm", new Casa());
         return "casa";
     }
 
     @PostMapping("/guardar/casa")
     public String guardar(@ModelAttribute Casa casa){
         casaService.saveCasa(casa);
-        return "redirect/casa";
+        return "redirect:/casa";
     }
 
     @GetMapping("/editar/casa/{id}")
     public String editar(@PathVariable Integer id, Model model){
         Casa casa = casaService.getCasaById(id);
-        model.addAttribute("casa", casa);
+        model.addAttribute("casa", casaService.getAllCasa()); // lista
+        model.addAttribute("casaForm", casa);
+        model.addAttribute("tab", "editar");
         return "casa";
     }
 
     @PostMapping("/actualizar/casa/{id}")
     public String actualizar(@PathVariable Integer id, @ModelAttribute Casa casa){
         casaService.updateCasa(id, casa);
-        return "redirect/casa";
+        return "redirect:/casa";
 
     }
 }
