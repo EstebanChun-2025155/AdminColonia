802b3795eab6f6ca011b7998f5ba9b20fd1483b4
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-22 11:20:46 -0600

    refactor: se modifico el nombre de los metodos del CRUD

--- Colonia/src/main/java/Control/Colonia/Service/AccesosService.java
+++ Colonia/src/main/java/Control/Colonia/Service/AccesosService.java
@@ -4,11 +4,11 @@ import Control.Colonia.Entity.Accesos;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public interface AccesosService {
     List<Accesos> getAllAccesos();
     Accesos getAccesosById(Integer id);
-    Accesos saveAcceso(Accesos accesos) throws RuntimeException;
+    Accesos saveAcceso(Accesos accesos);
     Accesos updateAcceso(Integer id, Accesos accesos);
-    void deleteAccesos(Integer id);
+    void deleteAcceso(Integer id);
 }
