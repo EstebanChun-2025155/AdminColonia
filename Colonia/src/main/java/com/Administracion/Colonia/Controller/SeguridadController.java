aa9f83a91313e982d22ec8ed17b6d0cdb848e01e
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-04-21 21:50:16 -0600

    feat: Se establecio conexion con la base de datos

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/SeguridadController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/SeguridadController.java
@@ -2,68 +2,24 @@ package com.Administracion.Colonia.Controller;
 
 import com.Administracion.Colonia.Entity.Seguridad;
 import com.Administracion.Colonia.Service.SeguridadService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/Seguridad")
+@Controller
 public class SeguridadController {
-    private final SeguridadService seguridadService;
 
-    public SeguridadController(SeguridadService seguridadService){
-        this.seguridadService = seguridadService; }
+    @Autowired
+    private SeguridadService seguridadService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getSeguridadById(@PathVariable Integer id){
-        try {
-            Seguridad buscarId = seguridadService.getSeguridadById(id);
-            return new ResponseEntity<>(buscarId,HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
+    @GetMapping("/seguridad")
+    public String mostrarSeguridad(Model model){
+        List<Seguridad> lista = seguridadService.getAllSeguridad();
+        model.addAttribute("listaSeguridad", lista);
 
-    @PostMapping
-    public ResponseEntity<Object> createSeguridad(@Valid @RequestBody Seguridad seguridad, BindingResult br){
-        if  (br.hasErrors()){
-            return  ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
-        }
-        try{
-            Seguridad createSeguridad = seguridadService.saveSeguridad(seguridad);
-            return new ResponseEntity<>(createSeguridad, HttpStatus.CREATED);
-        }catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteSeguridad(@PathVariable Integer id) {
-        try {
-            if (seguridadService.getSeguridadById(id) == null ) {
-                return ResponseEntity.status(404).body("No existe el empleado de seguridad");
-            }
-            seguridadService.deleteSeguridad(id);
-            return ResponseEntity.status(202).build();
-        }catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error al eliminar al empleado de seguridad");
-        }
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updateSeguridad( @PathVariable Integer id, @RequestBody @Valid Seguridad seguridad, BindingResult br) {
-        if (br.hasErrors()){
-            return ResponseEntity.badRequest().body(br.getAllErrors().get(0).getDefaultMessage());
-        }
-        try {
-            Seguridad actualizado = seguridadService.updateSeguridad(id, seguridad);
-            return ResponseEntity.ok(actualizado);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        return "VistaSeguridad";
     }
 }
