34083b4fe6013650a25642d87d35a0270c97a377
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-04-25 16:29:06 -0600

    feat: Se crearon metodos del CRUD para la entidad Residentes

--- Colonia/src/main/java/Control/Colonia/Controller/ResidenteController.java
+++ Colonia/src/main/java/Control/Colonia/Controller/ResidenteController.java
@@ -18,6 +18,50 @@ public class ResidenteController {
     public String mostrarResidente(Model model){
         List<Residente> lista = residenteService.getAllResidente();
         model.addAttribute("residente", lista);
+        model.addAttribute("residenteForm", new Residente());
+        return "residente";
+    }
+
+    @PostMapping("/eliminar/residente/{id}")
+    public String eliminar(@PathVariable Integer id){
+        residenteService.deleteResidente(id);
+        return "redirect:/residente";
+    }
+
+    @GetMapping("/nuevo/residente")
+    public String nuevoResidente(Model model){
+        model.addAttribute("residenteForm", new Residente());
+        return "residente";
+    }
+
+    @PostMapping("/guardar/residente")
+    public String guardarResidente(@ModelAttribute Residente residente){
+        residenteService.saveResidente(residente);
+        return "redirect:/residente";
+    }
+
+    @GetMapping("/editar/residente/{id}")
+    public String editarResidente(@PathVariable Integer id, Model model){
+        Residente residente = residenteService.getResidenteById(id);
+        model.addAttribute("residente", residenteService.getAllResidente());
+        model.addAttribute("residenteForm", residente);
+        model.addAttribute("tab", "editar");
+        return "residente";
+    }
+
+    @PostMapping("/actualizar/residente/{id}")
+    public String actualizarResidente(@PathVariable Integer id, @ModelAttribute Residente residente){
+        residenteService.updateResidente(id, residente);
+        return "redirect:/residente";
+
+    }
+
+    @GetMapping("/buscar/residente")
+    public String buscarResidente(@RequestParam Integer id, Model model){
+        Residente residente = residenteService.getResidenteById(id);
+
+        model.addAttribute("residente", List.of(residente));
+        model.addAttribute("residenteForm", new Residente());
 
         return "residente";
     }
