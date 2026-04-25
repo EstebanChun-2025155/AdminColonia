ec813c1c2257db4fcba36214741ac1a4b9188b4d
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-04-24 22:00:06 -0600

    refactor: Se modifico conexión con la vista seguridad

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/SeguridadController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/SeguridadController.java
@@ -16,10 +16,36 @@ public class SeguridadController {
     private SeguridadService seguridadService;
 
     @GetMapping("/seguridad")
-    public String mostrarSeguridad(Model model){
-        List<Seguridad> lista = seguridadService.getAllSeguridad();
-        model.addAttribute("listaSeguridad", lista);
-
+    public String mostrarSeguridad(Model model) {
+        model.addAttribute("listaSeguridad", seguridadService.getAllSeguridad());
+        model.addAttribute("seguridadForm", new Seguridad());
         return "VistaSeguridad";
+    }
+
+    @PostMapping("/seguridad/eliminar/{id}")
+    public String eliminar(@PathVariable Integer id) {
+        seguridadService.deleteSeguridad(id);
+        return "redirect:/seguridad";
+    }
+
+    @PostMapping("/seguridad/guardar")
+    public String guardar(@ModelAttribute Seguridad seguridad) {
+        seguridadService.saveSeguridad(seguridad);
+        return "redirect:/seguridad";
+    }
+
+    @GetMapping("/seguridad/editar/{id}")
+    public String editar(@PathVariable Integer id, Model model) {
+        Seguridad seguridad = seguridadService.getSeguridadById(id);
+        model.addAttribute("listaSeguridad", seguridadService.getAllSeguridad());
+        model.addAttribute("seguridadForm", seguridad);
+        model.addAttribute("tab", "editar");
+        return "VistaSeguridad";
+    }
+
+    @PostMapping("/seguridad/actualizar")
+    public String actualizar(@ModelAttribute Seguridad seguridad) {
+        seguridadService.saveSeguridad(seguridad);
+        return "redirect:/seguridad";
     }
 }
