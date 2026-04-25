9127b36e36702ab5900017a6a27c0a22a11c01f6
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-04-24 21:17:33 -0600

    refactor: Se modifico una parte del controller limpieza

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/LimpiezaController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/LimpiezaController.java
@@ -22,14 +22,12 @@ public class LimpiezaController {
         return "VistaLimpieza";
     }
 
-    // Ruta corregida para eliminar
     @PostMapping("/limpieza/eliminar/{id}")
     public String eliminar(@PathVariable Integer id) {
         limpiezaService.deleteLimpieza(id);
         return "redirect:/limpieza";
     }
 
-    // Ruta corregida para guardar
     @PostMapping("/limpieza/guardar")
     public String guardar(@ModelAttribute Limpieza limpieza) {
         limpiezaService.saveLimpieza(limpieza);
@@ -45,10 +43,9 @@ public class LimpiezaController {
         return "VistaLimpieza";
     }
 
-    // Ruta corregida para actualizar (quitamos el {id} de la URL porque ya viene dentro del objeto limpiezaForm)
     @PostMapping("/limpieza/actualizar")
     public String actualizar(@ModelAttribute Limpieza limpieza) {
-        limpiezaService.saveLimpieza(limpieza); // saveLimpieza suele servir para actualizar si el ID ya existe
+        limpiezaService.saveLimpieza(limpieza);
         return "redirect:/limpieza";
     }
 }
