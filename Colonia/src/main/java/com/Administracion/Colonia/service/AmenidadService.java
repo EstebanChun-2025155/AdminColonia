91f1b3ef58a19b89aa67e9a836944d7ba38af586
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-26 19:57:22 -0600

    refactor: Ajustes en la interfaz del servicio de amenidades

--- Colonia/src/main/java/com/Administracion/Colonia/service/AmenidadService.java
+++ Colonia/src/main/java/com/Administracion/Colonia/service/AmenidadService.java
@@ -1,24 +1,18 @@
 package com.Administracion.Colonia.service;
 
 import com.Administracion.Colonia.entity.Amenidad;
-import org.jspecify.annotations.Nullable;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface AmenidadService {
 
     List<Amenidad> getAllAmenidad();
 
-    Amenidad getAmenidadByid(Integer id);
+    Amenidad getAmenidadById(Integer id);
 
     Amenidad saveAmenidad(Amenidad amenidad) throws RuntimeException;
 
     Amenidad updateAmenidad(Integer id, Amenidad amenidad);
 
     void deleteAmenidad(Integer id);
-
-    @Nullable Object getAmenidadById(Integer id);
 }
-
