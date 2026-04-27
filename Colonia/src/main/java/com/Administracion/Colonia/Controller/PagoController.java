068c3e3bfd2b758f14918512c3d04e380ffaa105
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-04-26 22:21:57 -0600

    feat: Se agregaron y cambiaron los endpoints de Pago

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/PagoController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/PagoController.java
@@ -17,8 +17,44 @@ public class PagoController {
 
     @GetMapping("/pago")
     public String mostrarPago (Model model) {
-        List<Pago> pagos = pagoService.getAllPago();
-        model.addAttribute("pagos", pagos);
+        model.addAttribute("pagos", pagoService.getAllPago());
+        model.addAttribute("pagoNuevo", new Pago());
+        model.addAttribute("panelActivo", "consultar");
         return "VistaPago";
+    }
+
+    @GetMapping("/pago/nueva")
+    public String nuevoPago(Model model) {
+        model.addAttribute("pagos", pagoService.getAllPago());
+        model.addAttribute("pagoNuevo", new Pago());
+        model.addAttribute("panelActivo", "registrar");
+        return "VistaPago";
+    }
+
+    @PostMapping("/pago/nueva")
+    public String guardarPago(@ModelAttribute Pago pago) {
+        pagoService.savePago(pago);
+        return "redirect:/pago";
+    }
+
+    @GetMapping("/pago/editar/{id}")
+    public String editarPago(@PathVariable Integer id, Model model) {
+        model.addAttribute("pagos", pagoService.getAllPago());
+        model.addAttribute("pagoNuevo", new Pago());
+        model.addAttribute("pagoEditar", pagoService.getPagoById(id));
+        model.addAttribute("panelActivo", "editar");
+        return "VistaPago";
+    }
+
+    @PostMapping("/pago/editar/{id}")
+    public String actualizarPago(@PathVariable Integer id, @ModelAttribute Pago pago) {
+        pagoService.updatePago(id, pago);
+        return "redirect:/pago";
+    }
+
+    @GetMapping("/pago/eliminar/{id}")
+    public String eliminarPago(@PathVariable Integer id) {
+        pagoService.deletePago(id);
+        return "redirect:/pago";
     }
 }
