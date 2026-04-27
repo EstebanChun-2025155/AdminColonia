0a2cac5bb210e0dcec4fd4ea5088878dd422085b
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-04-27 17:59:21 -0600

    feat: Se agrego la clase LoginController

---
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.Administracion.Colonia.Controller;
+
+import com.Administracion.Colonia.Entity.Residente;
+import com.Administracion.Colonia.Service.ResidenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private ResidenteService residenteService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String validar(@RequestParam String nombreResidente, @RequestParam String dpiResidente, Model model, HttpSession session) {
+        Residente u = residenteService.login(nombreResidente, dpiResidente);
+        if (u != null && u.getPosicion().equalsIgnoreCase("activo")) {
+            session.setAttribute("usuarioLogueado", u);
+
+            return "redirect:/home";
+
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
