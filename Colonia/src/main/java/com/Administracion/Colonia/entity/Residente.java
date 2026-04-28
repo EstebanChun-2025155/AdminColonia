ffbff97d16e7cef1e6ab8eb6cef39e146a0dbea1
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-27 20:30:44 -0600

    feat: Se implemento la entity de Residentes

--- Colonia/src/main/java/com/Administracion/Colonia/entity/Residente.java
+++ Colonia/src/main/java/com/Administracion/Colonia/entity/Residente.java
@@ -1,4 +1,48 @@
 package com.Administracion.Colonia.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Residente")
 public class Residente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idResidente;
+
+    private String nombreResidente;
+    private String dpiResidente;
+    private String posicion;
+
+    public Integer getIdResidente() {
+        return idResidente;
+    }
+
+    public void setIdResidente(Integer idResidente) {
+        this.idResidente = idResidente;
+    }
+
+    public String getNombreResidente() {
+        return nombreResidente;
+    }
+
+    public void setNombreResidente(String nombreResidente) {
+        this.nombreResidente = nombreResidente;
+    }
+
+    public String getDpiResidente() {
+        return dpiResidente;
+    }
+
+    public void setDpiResidente(String dpiResidente) {
+        this.dpiResidente = dpiResidente;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posición) {
+        this.posicion = posición;
+    }
 }
