094b39bf0c62c9589e3d943e85e6ec39c23d431f
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-04-27 20:09:48 -0600

    feat: Se agrego validación de sesión de usuario al endpoint home

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/HomeController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/HomeController.java
@@ -1,18 +1,17 @@
 package com.Administracion.Colonia.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String inicio() {
-        return "redirect:/home";
-    }
-
     @GetMapping("/home")
-    public String mostrarHome () {
+    public String home(HttpSession session){
+        if(session.getAttribute("usuarioLogueado") == null){
+            return "redirect:/login";
+        }
         return "VistaHome";
     }
 
