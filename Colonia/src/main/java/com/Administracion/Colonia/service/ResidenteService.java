ef0a2e3c792b2bd32c8d36d1aea69042b0a04810
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-27 20:32:33 -0600

    feat: Se implemento ResidenteService al service

--- Colonia/src/main/java/com/Administracion/Colonia/service/ResidenteService.java
+++ Colonia/src/main/java/com/Administracion/Colonia/service/ResidenteService.java
@@ -1,4 +1,7 @@
 package com.Administracion.Colonia.service;
 
+import com.Administracion.Colonia.entity.Residente;
+
 public interface ResidenteService {
+    Residente login(String nombreResidente, String dpiResidente);
 }
