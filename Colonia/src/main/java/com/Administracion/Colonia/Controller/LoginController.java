71ef7682b93c5da61ab0cd567cdf747fe0e31f64
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-04-27 19:21:51 -0600

    feat: Se agregaron los endpoints de Login y validación de usuario

---
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.Administracion.Colonia.Controller;
+
+import com.Administracion.Colonia.Entity.Residente;
+import com.Administracion.Colonia.Service.ResidenteService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private ResidenteService residenteService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "VistaLogin";
+    }
+
+    @PostMapping("/login")
+    public String validar(@RequestParam String nombreResidente, @RequestParam String dpiResidente, Model model, HttpSession session) {
+        Residente u = residenteService.login(nombreResidente, dpiResidente);
+        if (u != null && u.getPosicion().equalsIgnoreCase("activo")) {
+            session.setAttribute("usuarioLogueado", u);
+
+            return "redirect:/home";
+
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "VistaLogin";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/login";
+    }
+
+}
