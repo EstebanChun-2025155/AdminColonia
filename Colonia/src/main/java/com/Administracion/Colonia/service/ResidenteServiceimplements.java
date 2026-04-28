10c871fdec51a738b327216dfea8994192f42087
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-27 20:34:03 -0600

    feat: Se implemento ResidentesServiceImplemts al service

--- Colonia/src/main/java/com/Administracion/Colonia/service/ResidenteServiceimplements.java
+++ Colonia/src/main/java/com/Administracion/Colonia/service/ResidenteServiceimplements.java
@@ -1,4 +1,18 @@
 package com.Administracion.Colonia.service;
 
-public class ResidenteServiceimplements {
+import com.Administracion.Colonia.entity.Residente;
+import com.Administracion.Colonia.repository.ResidenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResidenteServiceimplements implements ResidenteService {
+
+    @Autowired
+    private ResidenteRepository residenteRepository;
+
+    @Override
+    public Residente login(String nombreResidente, String dpiResidente) {
+        return residenteRepository.findByNombreResidenteAndDpiResidente(nombreResidente, dpiResidente);
+    }
 }
