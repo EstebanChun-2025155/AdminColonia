f551f868f1928c5b382f7ff9eae08f2a50bfc775
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-04-28 11:55:09 -0600

    feat: se agrego la interface ResidentesRepository para el funcionamiento del login

---
+++ Colonia/src/main/java/Control/Colonia/Repository/ResidenteRepository.java
@@ -0,0 +1,12 @@
+package Control.Colonia.Repository;
+
+import Control.Colonia.Entity.Residente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResidenteRepository extends JpaRepository<Residente, Integer> {
+
+    Residente findByNombreResidenteAndDpiResidente(String nombreResidente,
+                                                   String dpiResidente);
+}
