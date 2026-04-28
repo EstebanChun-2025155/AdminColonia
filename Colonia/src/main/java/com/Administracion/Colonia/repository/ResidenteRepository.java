f16b49cbe85f2903f83ce02c4d70408485bc8abc
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-04-27 20:31:53 -0600

    feat: Se implemento ResidenteRepository al repository

--- Colonia/src/main/java/com/Administracion/Colonia/repository/ResidenteRepository.java
+++ Colonia/src/main/java/com/Administracion/Colonia/repository/ResidenteRepository.java
@@ -1,4 +1,12 @@
 package com.Administracion.Colonia.repository;
 
-public interface ResidenteRepository {
+import com.Administracion.Colonia.entity.Residente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResidenteRepository extends JpaRepository<Residente, Integer> {
+
+    Residente findByNombreResidenteAndDpiResidente(String nombreResidente,
+                                                   String dpiResidente);
 }
