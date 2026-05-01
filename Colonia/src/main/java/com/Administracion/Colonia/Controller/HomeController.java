aae16156814362b613a238f7e349034eb55c2d6a
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-04-30 20:54:39 -0600

    feat: Se agrego el control de acceso por rol en HomeController

--- Colonia/src/main/java/com/Administracion/Colonia/Controller/HomeController.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.Administracion.Colonia.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,8 +14,17 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String mostrarHome () {
+    public String mostrarHome(HttpSession session, Model model) {
+
+        String tipo = (String) session.getAttribute("tipoUsuario");
+
+        if (tipo == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("isResidente", tipo.equals("RESIDENTE"));
+        model.addAttribute("isSeguridad", tipo.equals("SEGURIDAD"));
+
         return "VistaHome";
     }
-
-    }
+}
