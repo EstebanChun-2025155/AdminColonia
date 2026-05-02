6345171220335ef055931a39b19d8f7387881478
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 12:56:44 -0600

    feat: Se establecio la comunicacion con la db para la entidad Seguridad

---
+++ Colonia/src/main/java/Control/Colonia/Repository/SeguridadRepository.java
@@ -0,0 +1,16 @@
+package Control.Colonia.Repository;
+
+import Control.Colonia.Entity.Seguridad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SeguridadRepository extends JpaRepository<Seguridad, Integer> {
+    Boolean existsByNombreAndPuestoAndJornadaAndSalarioAndTelefono(
+            String nombre,
+            String puesto,
+            String jornada,
+            Double salario,
+            String telefono
+    );
+}
