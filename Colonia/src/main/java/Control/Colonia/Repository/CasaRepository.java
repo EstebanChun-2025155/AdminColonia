1f4fbe69a9c8a6bfe9c18a41ee068e891bf0caf4
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 12:55:15 -0600

    feat: Se establecio la comunicacion con la db para la entidad Casa

---
+++ Colonia/src/main/java/Control/Colonia/Repository/CasaRepository.java
@@ -0,0 +1,16 @@
+package Control.Colonia.Repository;
+
+import Control.Colonia.Entity.Casa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CasaRepository extends JpaRepository<Casa, Integer>{
+    Boolean existsByNoDeCasaAndDireccionAndEstadoAndPropietarioAndPrecioCasa(
+            String noDeCasa,
+            String direccion,
+            String estado,
+            String propietario,
+            Double precioCasa
+    );
+}
