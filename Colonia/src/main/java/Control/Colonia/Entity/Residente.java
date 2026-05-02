bb4d77c5dff80873c1348412c6bdb4091475462f
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 12:40:12 -0600

    feat: Se cargaron los atributos de las entidad Residente

---
+++ Colonia/src/main/java/Control/Colonia/Entity/Residente.java
@@ -0,0 +1,90 @@
+package Control.Colonia.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "Residente")
+public class Residente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id_Residente")
+    private Integer idResidente;
+
+    @NotBlank(message = "El nombre del residente es obligatorio")
+    @Column(name = "nombre_Residente")
+    private String nombreResidente;
+
+    @NotBlank(message = "El DPI es obligatorio")
+    @Pattern(regexp = "^[0-9]{13}$", message = "El DPI debe contener exactamente 13 números")
+    @Column(name = "dpi_Residente")
+    private String dpiResidente;
+
+    @NotBlank(message = "El teléfono es obligatorio")
+    @Pattern(regexp = "^[0-9]{8}$", message = "El telefono debe contener exactamente 8 numeros")
+    @Column(name = "telefono_Residente")
+    private String telefonoResidente;
+
+    @NotBlank(message = "La posición es obligatoria")
+    @Pattern(regexp = "^(activo|inactivo)$",
+            message = "La posicion solo es valida bajo los dominios: activo, inactivo")
+    @Column(name = "posicion")
+    private String posicion;
+
+    @NotNull(message = "Los campos no pueden estar vacios")
+    @Positive(message = "El ID de casa debe ser un número positivo")
+    @Column(name = "id_Casa")
+    private Integer idCasa;
+
+    public Integer getIdResidente() {
+        return idResidente;
+    }
+
+    public void setIdResidente(Integer idResidente) {
+        this.idResidente = idResidente;
+    }
+
+    public String getNombreResidente() {
+        return nombreResidente;
+    }
+
+    public void setNombreResidente(String nombreResidente) {
+        this.nombreResidente = nombreResidente;
+    }
+
+    public String getDpiResidente() {
+        return dpiResidente;
+    }
+
+    public void setDpiResidente(String dpiResidente) {
+        this.dpiResidente = dpiResidente;
+    }
+
+    public String getTelefonoResidente() {
+        return telefonoResidente;
+    }
+
+    public void setTelefonoResidente(String telefonoResidente) {
+        this.telefonoResidente = telefonoResidente;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public Integer getIdCasa() {
+        return idCasa;
+    }
+
+    public void setIdCasa(Integer idCasa) {
+        this.idCasa = idCasa;
+    }
+}
