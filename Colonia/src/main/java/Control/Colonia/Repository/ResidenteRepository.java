e8a9322c1b33d32357e3ae617fb8a392bd037f03
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 12:56:25 -0600

    feat: Se establecio la comunicacion con la db para la entidad Residente

---
+++ Colonia/src/main/java/Control/Colonia/Repository/ResidenteRepository.java
@@ -0,0 +1,18 @@
+package Control.Colonia.Repository;
+
+import Control.Colonia.Entity.Residente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResidenteRepository extends JpaRepository<Residente, Integer> {
+    Boolean existsByNombreResidenteAndDpiResidenteAndTelefonoResidenteAndPosicionAndIdCasa(
+            String nombreResidente,
+            String dpiResidente,
+            String telefonoResidente,
+            String posicion,
+            Integer idCasa
+    );
+
+    Residente findByNombreResidenteAndDpiResidente(String nombreResidente, String dpiResidente);
+}
