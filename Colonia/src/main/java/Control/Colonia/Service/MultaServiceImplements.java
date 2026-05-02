accaafa7d8a92c835ab65530fafaf0312d32375f
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 13:02:23 -0600

    feat: Se cargaron las clases Service de la entidad Multa con la lógica del CRUD

---
+++ Colonia/src/main/java/Control/Colonia/Service/MultaServiceImplements.java
@@ -0,0 +1,67 @@
+package Control.Colonia.Service;
+
+import Control.Colonia.Entity.Multa;
+import Control.Colonia.Repository.MultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MultaServiceImplements implements MultaService {
+
+    @Autowired
+    private MultaRepository multaRepository;
+
+    @Override
+    public List<Multa> getAllMulta() { return multaRepository.findAll(); }
+
+    @Override
+    public Multa getMultaById(Integer id) {
+        return multaRepository.findById(id).orElseThrow(() -> new RuntimeException("Multa no encontrada"));
+    }
+
+    @Override
+    public Multa saveMulta(Multa multa) throws RuntimeException {
+        try {
+            if (multaRepository.existsByMontoAndDescripcionAndFechaEmisionAndEstadoAndTipoPersona(
+                    multa.getMonto(),
+                    multa.getDescripcion(),
+                    multa.getFechaEmision(),
+                    multa.getEstado(),
+                    multa.getTipoPersona())) {
+                throw new RuntimeException("Ya existe una multa con estos datos");
+            }
+            return multaRepository.save(multa);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Multa updateMulta(Integer id, Multa multa) {
+        Multa existingMulta = multaRepository.findById(id).orElseThrow(() -> new RuntimeException("La multa no existe"));
+
+        if (multaRepository.existsByMontoAndDescripcionAndFechaEmisionAndEstadoAndTipoPersona(
+                multa.getMonto(),
+                multa.getDescripcion(),
+                multa.getFechaEmision(),
+                multa.getEstado(),
+                multa.getTipoPersona())) {
+            throw new RuntimeException("Ya existe una multa con estos datos");
+        }
+
+        existingMulta.setMonto(multa.getMonto());
+        existingMulta.setDescripcion(multa.getDescripcion());
+        existingMulta.setFechaEmision(multa.getFechaEmision());
+        existingMulta.setEstado(multa.getEstado());
+        existingMulta.setTipoPersona(multa.getTipoPersona());
+
+        return multaRepository.save(existingMulta);
+    }
+
+    @Override
+    public void deleteMulta(Integer id) {
+        multaRepository.deleteById(id);
+    }
+}
