4e254c92c69dbf07c0de037d16810b6b5b737144
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-02 13:01:55 -0600

    feat: Se cargaron las clases Service de la entidad Limpieza con la lógica del CRUD

---
+++ Colonia/src/main/java/Control/Colonia/Service/LimpiezaServiceImplements.java
@@ -0,0 +1,73 @@
+package Control.Colonia.Service;
+
+import Control.Colonia.Entity.Limpieza;
+import Control.Colonia.Repository.LimpiezaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LimpiezaServiceImplements implements LimpiezaService {
+
+    @Autowired
+    private LimpiezaRepository limpiezaRepository;
+
+    @Override
+    public List<Limpieza> getAllLimpieza() {
+        return limpiezaRepository.findAll();
+    }
+
+    @Override
+    public Limpieza getLimpiezaById(Integer id) {
+        return limpiezaRepository.findById(id).orElseThrow(() -> new RuntimeException("Este empleado de limpieza no existe"));
+    }
+
+    @Override
+    public Limpieza saveLimpieza(Limpieza limpieza) throws RuntimeException {
+        try {
+            if (limpiezaRepository.existsByNombreAndPuestoAndJornadaAndSalarioAndTelefono(
+                    limpieza.getNombre(),
+                    limpieza.getPuesto(),
+                    limpieza.getJornada(),
+                    limpieza.getSalario(),
+                    limpieza.getTelefono())) {
+                throw new RuntimeException("Ya existe un empleado de limpieza con estos datos");
+            }
+            return limpiezaRepository.save(limpieza);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Limpieza updateLimpieza(Integer id, Limpieza limpieza) {
+        Limpieza existingLimpieza = limpiezaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("El empleado de limpieza no existe"));
+
+        if (limpiezaRepository.existsByNombreAndPuestoAndJornadaAndSalarioAndTelefono(
+                limpieza.getNombre(),
+                limpieza.getPuesto(),
+                limpieza.getJornada(),
+                limpieza.getSalario(),
+                limpieza.getTelefono())){
+            throw new RuntimeException("Ya existe un empleado de limpieza con estos datos");
+        }
+
+        existingLimpieza.setNombre(limpieza.getNombre());
+        existingLimpieza.setPuesto(limpieza.getPuesto());
+        existingLimpieza.setJornada(limpieza.getJornada());
+        existingLimpieza.setSalario(limpieza.getSalario());
+        existingLimpieza.setTelefono(limpieza.getTelefono());
+
+        return limpiezaRepository.save(existingLimpieza);
+    }
+
+    @Override
+    public void deleteLimpieza(Integer id) {
+        if (!limpiezaRepository.existsById(id)){
+            throw new RuntimeException("Este id no existe");
+        }
+        limpiezaRepository.deleteById(id);
+    }
+}
