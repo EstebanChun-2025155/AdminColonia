4d0a1bac67a50038f3442f664740a47e13abceb3
JosueChan-2022047 <jchan-2022047@kinal.edu.gt>
2026-05-03 17:19:06 -0600

    docs: Se actualizo el mapeo de entidades y metodos del repositorio

--- Colonia/src/main/java/com/Administracion/Colonia/repository/VehiculoRepository.java
+++ Colonia/src/main/java/com/Administracion/Colonia/repository/VehiculoRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Integer> {
-    // Solo necesitamos verificar la placa porque es el único campo UNIQUE
     boolean existsByPlaca(String placa);
 }
