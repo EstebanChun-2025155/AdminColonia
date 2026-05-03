10470430044ee5f1ce6a7e56688d24052c013381
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-05-03 16:30:13 -0600

    feat: Se establecio la comunicación con la db para la entidad Reporte

---
+++ Colonia/src/main/java/Control/Colonia/Repository/ReporteRepository.java
@@ -0,0 +1,22 @@
+package Control.Colonia.Repository;
+
+import Control.Colonia.Entity.Reporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ReporteRepository extends JpaRepository<Reporte, Integer> {
+    Boolean existsByIdResidenteReportanteAndIdResidenteReportadoAndTipoReporteAndDescripcionAndFechaReporteAndEstado(
+            Integer idResidenteReportante,
+            Integer idResidenteReportado,
+            String tipoReporte,
+            String descripcion,
+            LocalDate fechaReporte,
+            String estado
+    );
+
+    List<Reporte> findByIdResidenteReportado(Integer idResidenteReportado);
+}
