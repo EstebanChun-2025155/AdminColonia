9a5061cd5bf2f3cefb581535aa1a845d4d0a89c0
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-05-05 08:17:32 -0600

    fix: Se corrigio la anotacion de la clase LimpiezaRepository

--- Colonia/src/main/java/com/Administracion/Colonia/Repository/LimpiezaRepository.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Repository/LimpiezaRepository.java
@@ -2,9 +2,10 @@ package com.Administracion.Colonia.Repository;
 
 import com.Administracion.Colonia.Entity.Limpieza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface LimpiezaRepository extends JpaRepository<Limpieza, Integer> {
     Boolean existsByNombreAndPuestoAndJornadaAndSalarioAndTelefono(
             String nombre,
