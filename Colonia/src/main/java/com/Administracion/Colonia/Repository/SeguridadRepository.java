4118cf0cb2713857f70220901ea5faa257c247b7
wilfred <wdepaz-2025275@kinal.edu.gt>
2026-05-05 08:47:03 -0600

    fix: Se corrigio la anotacion de la clase SeguridadRepository

--- Colonia/src/main/java/com/Administracion/Colonia/Repository/SeguridadRepository.java
+++ Colonia/src/main/java/com/Administracion/Colonia/Repository/SeguridadRepository.java
@@ -2,9 +2,10 @@ package com.Administracion.Colonia.Repository;
 
 import com.Administracion.Colonia.Entity.Seguridad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public interface SeguridadRepository extends JpaRepository<Seguridad, Integer> {
     Boolean existsByNombreAndPuestoAndJornadaAndSalarioAndTelefonoSeguridadAndDpiSeguridad(
             String nombre,
